67da08f161d3c7bc010567346f9d5703cbf8b008
Mark Brazee <m.brazee@comcast.net>
2020-11-24 10:19:30 -0700

    working App-js

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import Wrapper from "./components/Wrapper";
 import Employees from "./employees.json";
 import EmployeeList from "./components/EmployeeList";
 import Title from "./components/Title";
 
+// Setting this.state.employees to employees json array
+class App extends Component {
+  state = {
+    Employees
+  };
 
-function App() {
-  return (
-   <Wrapper>
-     <Title>Employee Directory</Title>
-     {Employees.map(employee => (
-       <EmployeeList
-          firstName={employee.firstName}
-          lastName={employee.lastName}
-          email={employee.email}
-          phone={employee.phone}
-          occupation={employee.occupation}
-          residence={employee.residence}
-        />
-      ))}
-   </Wrapper>
-  );
+
+  filterEmployee = id => {
+    
+  }
+
+
+
+  // Map over this.state.friends and render a Employee component for each employee object
+  App() {
+    return (
+    <Wrapper>
+      <Title>Employee Directory</Title>
+      {Employees.map(employee => (
+        <EmployeeList
+            firstName={employee.firstName}
+            lastName={employee.lastName}
+            email={employee.email}
+            phone={employee.phone}
+            occupation={employee.occupation}
+            residence={employee.residence}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
+
 
 export default App;
