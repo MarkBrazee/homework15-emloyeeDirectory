0f3e0b24af734e3fa1029c921574a4a87bb40887
Mark Brazee <m.brazee@comcast.net>
2020-11-24 15:19:28 -0700

    revised file structure

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper";
+import Employees from "./employees.json";
+import EmployeeList from "./components/EmployeeList";
+import Title from "./components/Title";
+
+// Map over this.state.friends and render a Employee component for each employee object
+function App() {
+  return (
+    <Wrapper>
+      <Title>Employee Directory</Title>
+      {Employees.map((employee) => (
+        <EmployeeList
+          firstName={employee.firstName}
+          lastName={employee.lastName}
+          email={employee.email}
+          phone={employee.phone}
+          occupation={employee.occupation}
+          residence={employee.residence}
+        />
+      ))}
+    </Wrapper>
+  );
+}
+
+export default App;
